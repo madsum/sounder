1c07f47b7d7239531a30562cf5e3af08c73ceb7a
renaudbaivier@gmail.com <renaudbaivier@gmail.com@ab872372-0214-28b5-75a8-89f0dc205f14>
2012-02-16 15:12:04 +0000

    Ajout de la class Player. Ajout des images. Strings pour Player. Layout pour Player.

--- trunk/Sounder/src/com/renaudbaivier/sounder/PlayerActivity.java
+++ trunk/Sounder/src/com/renaudbaivier/sounder/PlayerActivity.java
@@ -1,15 +1,13 @@
 package com.renaudbaivier.sounder;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 
-public class SounderActivity extends Activity {
+public class PlayerActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.player);
-        
     }
 }
