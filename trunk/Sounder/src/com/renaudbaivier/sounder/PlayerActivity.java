2ce37e6a4b0cb5c3e7b71a8c8bcf7a9678cf85cf
renaudbaivier@gmail.com <renaudbaivier@gmail.com@ab872372-0214-28b5-75a8-89f0dc205f14>
2012-02-17 16:57:45 +0000

    Création du player > Lecture et Pause fonctionnelle. Bouton de DEV pour passer sur le PlayerActivity.
    !! Attention à ne pas committer TOUS les fichiers (.classpath, .project, dossier bin, ...) !!

--- trunk/Sounder/src/com/renaudbaivier/sounder/PlayerActivity.java
+++ trunk/Sounder/src/com/renaudbaivier/sounder/PlayerActivity.java
@@ -1,13 +1,54 @@
 package com.renaudbaivier.sounder;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class PlayerActivity extends Activity {
     /** Called when the activity is first created. */
+	
+	// Lecture MP3
+	MediaPlayer mediaPlayer;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.player);
+        
+        // Récupération du fichier
+        // Ficher et chemin de TEST
+        mediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.mp3); 
+    }
+    
+    // Appui sur touche back
+    // A mettre dans TOUTE les Activity !
+    public void onBackPressed() {
+    	
+    }
+    
+    // Lecture du MP3
+    public void play(View v) {
+    	mediaPlayer.start();
+    	ImageView pause = (ImageView) findViewById(R.id.pause);
+    	pause.setVisibility(View.VISIBLE);
+    	ImageView play = (ImageView) findViewById(R.id.play);
+    	play.setVisibility(View.INVISIBLE);
+    }
+    
+    // Pause du MP3
+    public void pause(View v) {
+    	mediaPlayer.pause();
+    	ImageView pause = (ImageView) findViewById(R.id.pause);
+    	pause.setVisibility(View.INVISIBLE);
+    	ImageView play = (ImageView) findViewById(R.id.play);
+    	play.setVisibility(View.VISIBLE);
+    }
+    
+    // Stop du MP3
+    public void stop(View v) {
+    	mediaPlayer.stop();
     }
 }
