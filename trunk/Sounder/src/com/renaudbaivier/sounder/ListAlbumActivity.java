69c5ab5201ed6c0a8386b4beebdd8790e1c7da97
lyanwoah@gmail.com <lyanwoah@gmail.com@ab872372-0214-28b5-75a8-89f0dc205f14>
2012-03-01 13:42:26 +0000

    Derniere version de liste des Albums: balance un tableau de songs ainsi que l'index de la position à laquelle on doit commencer la lecture
    enjoy ^^

--- trunk/Sounder/src/com/renaudbaivier/sounder/ListAlbumActivity.java
+++ trunk/Sounder/src/com/renaudbaivier/sounder/ListAlbumActivity.java
@@ -5,13 +5,17 @@ import java.util.HashMap;
 import java.util.List;
 
 import android.app.ExpandableListActivity;
+import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.ExpandableListView;
+import android.widget.ListAdapter;
 import android.widget.SimpleExpandableListAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
 
 	public class ListAlbumActivity extends ExpandableListActivity {
 	    /** Called when the activity is first created. */
@@ -47,11 +51,12 @@ import android.widget.SimpleExpandableListAdapter;
     	 ArrayList<HashMap<String, String>> result = new ArrayList<HashMap<String, String>>();
     	 
  		String[] proj = { 
- 				  MediaStore.Audio.Media.ALBUM_ID,
+ 				 // MediaStore.Audio.Media.ALBUM_ID,
   				  MediaStore.Audio.Media.ALBUM,
   				  MediaStore.Audio.Media.DISPLAY_NAME,
   				  };
- 		Cursor musiccursor = managedQuery(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,proj, null, null, MediaStore.Audio.Media.ALBUM_ID);
+ 		//String where = "1=1) GROUP BY (ALBUM_ID";
+ 		Cursor musiccursor = managedQuery(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,proj, null, null, MediaStore.Audio.Media.ALBUM);
  		int count = musiccursor.getCount();
  		
  		ArrayList<HashMap<String, String>> secList = new ArrayList<HashMap<String, String>>();
@@ -97,7 +102,8 @@ import android.widget.SimpleExpandableListAdapter;
 				  MediaStore.Audio.Media.ALBUM_ID,
 				  MediaStore.Audio.Media.ALBUM,
  				  };
-		Cursor musiccursor = managedQuery(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,proj, null, null,MediaStore.Audio.Media.ALBUM_ID);
+		String where = "1=1) GROUP BY (ALBUM";
+		Cursor musiccursor = managedQuery(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,proj, where, null,MediaStore.Audio.Media.ALBUM);
 		int count = musiccursor.getCount();
 		for(int i=0; i<count; i++)
 		{
@@ -106,7 +112,7 @@ import android.widget.SimpleExpandableListAdapter;
 			HashMap<String, String> album = new HashMap<String, String>();
 			album.put( "Group Item", groupepalbum );
 			Log.d("album groupe", album.toString());
-			if(!result.contains(album))
+			//if(!result.contains(album))
 			result.add(album);
 			
 					
@@ -119,14 +125,54 @@ import android.widget.SimpleExpandableListAdapter;
 	    super.onContentChanged();
     }
     /* This function is called on each child click */
-    public boolean onChildClick( ExpandableListView parent, View v, int groupPosition,int childPosition,long id) {
-    	System.out.println("Inside onChildClick at groupPosition = " + groupPosition +" Child clicked at position " + childPosition);
+    public boolean onChildClick( ExpandableListView parent, View v, int groupPosition,int childPosition,long id) 
+    {
+    	String[] proj = { MediaStore.Audio.Media._ID,
+				  MediaStore.Audio.Media.DATA,
+				  MediaStore.Audio.Media.DISPLAY_NAME,
+				  MediaStore.Audio.Media.ARTIST,
+				  MediaStore.Audio.Media.ALBUM,
+				  MediaStore.Audio.Media.TITLE,
+				//  MediaStore.Audio.Media.ALBUM_ART
+			};
+    		HashMap albums = (HashMap)parent.getExpandableListAdapter().getGroup(groupPosition);
+    		String myAlbum = (String) albums.get("Group Item");
+    		String where="album = "+myAlbum;
+    		
+			Cursor musiccursor = managedQuery(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,proj, MediaStore.Audio.Media.ALBUM + " like ? ", new String[] {myAlbum}, MediaStore.Audio.Media.ALBUM);
+			int count = musiccursor.getCount();
+			Song[] songs = new Song[count];
+			for(int i=0;i<count;i++)
+			{
+	    	 musiccursor.moveToPosition(i);         
+	         int music_column_index =  musiccursor.getColumnIndexOrThrow(MediaStore.Audio.Media.TITLE);
+	         String title = musiccursor.getString(music_column_index);
+	         music_column_index =  musiccursor.getColumnIndexOrThrow(MediaStore.Audio.Media.ALBUM);
+	         String album = musiccursor.getString(music_column_index);
+	         music_column_index =  musiccursor.getColumnIndexOrThrow(MediaStore.Audio.Media.ARTIST);
+	         String artist = musiccursor.getString(music_column_index);
+	         music_column_index = musiccursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DATA);
+	         String filename = musiccursor.getString(music_column_index);
+	         songs[i] = new Song(title, album, artist, filename);
+	         //Toast.makeText(getApplicationContext(),"album: "+ album+" zik: "+ title, Toast.LENGTH_SHORT).show();
+			}          
+         
+			//Toast.makeText(getApplicationContext(),"musique selectionne: "+ songs[childPosition].getTitre(), Toast.LENGTH_SHORT).show();
+    	
+    	Intent t = new Intent(getApplicationContext(),com.renaudbaivier.sounder.PlayerActivity.class);
+    	t.putExtra("function","addMultiple");
+    	t.putExtra("newlist",songs);
+    	t.putExtra("startIndex",childPosition);
+    	startActivity(t);
+    	
+    	//System.out.println("Inside onChildClick at groupPosition = " + groupPosition +" Child clicked at position " + childPosition);
     	return true;
     }
 
     /* This function is called on expansion of the group */
     public void  onGroupExpand  (int groupPosition) {
     	try{
+    		Log.d("onGroupExpand",""+groupPosition);
     		 System.out.println("Group exapanding Listener => groupPosition = " + groupPosition);
     	}catch(Exception e){
     		System.out.println(" groupPosition Errrr +++ " + e.getMessage());
